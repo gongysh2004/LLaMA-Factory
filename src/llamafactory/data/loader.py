0acf85c9b25aaf86dad252ef1fdbce9afe67d0b4
gong yong sheng <gong_ys2025@126.com>
2025-12-24 14:54:05 +0800

    add first sample 打造基于Qwen3-vl-30B-A3B-Instruct的中文作文自动阅卷助手

--- src/llamafactory/data/loader.py
+++ src/llamafactory/data/loader.py
@@ -143,7 +143,7 @@ def _load_single_dataset(
             dataset = dataset.to_iterable_dataset(num_shards=training_args.dataloader_num_workers)
 
     if dataset_attr.num_samples is not None and not data_args.streaming:
-        target_num = dataset_attr.num_samples
+        target_num = int(dataset_attr.num_samples)
         indexes = np.random.permutation(len(dataset))[:target_num]  # all samples should be included
         target_num -= len(indexes)
         if target_num > 0:
